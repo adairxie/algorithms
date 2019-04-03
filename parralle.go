19e2e1634e180ac90541b79aa7c6ee8ebbddd6e5
xiehui <615810790@qq.com>
2019-04-03 09:53:39 +0800

    use sync.WaitGroup instead of time.Sleep

--- parralle.go
+++ parralle.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"time"
+    "sync"
 )
 
 func PrintStringSlice(s []string) {
@@ -14,13 +14,16 @@ func PrintStringSlice(s []string) {
 
 func parrell(data []string, chunk, parrell int, processor func([]string)) {
 	ch := make(chan struct{}, parrell)
+    var wg sync.WaitGroup
 
 	data_len := len(data)
 	for idx := 0; idx < data_len; idx += chunk {
+        wg.Add(1)
 		if idx+chunk >= data_len {
 			msg := data[idx:data_len]
 			ch <- struct{}{}
 			go func(msg []string) {
+                defer wg.Done()
 				processor(msg)
 				<-ch
 			}(msg)
@@ -28,16 +31,18 @@ func parrell(data []string, chunk, parrell int, processor func([]string)) {
 			msg := data[idx : idx+chunk]
 			ch <- struct{}{}
 			go func(msg []string) {
+                defer wg.Done()
 				processor(msg)
 				<-ch
 			}(msg)
 		}
 	}
+
+    wg.Wait()
 }
 
 func main() {
 	str := []string{"1", "2", "3", "4", "5", "6", "7"}
 	parrell(str, 3, 2, PrintStringSlice)
-	time.Sleep(time.Second * 1)
 	fmt.Println("end")
 }
